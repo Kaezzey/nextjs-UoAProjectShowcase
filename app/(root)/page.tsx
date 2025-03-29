f2af1b9f9904692e51df5105f1b606984a00a5fe
Kaezzey <ketkraft88@gmail.com>
2025-03-29 21:57:14 +1300

    Tailwind V4 workaround

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import * as tw from '../tailwind';
 
 export default function Home() {
   return (
     <>
-    <h1 className="heading">Home</h1>
+    <h1 className={tw.heading}>Home</h1>
     </>
   );
 }
